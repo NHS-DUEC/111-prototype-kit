1dee354ce449d9b034ad364481585289e2580d03
Paul Smith <mail@paulsmith.site>
2025-04-15 15:22:47 +0100

    remove unused filter

--- app/lib/filters/documentComponentOptions.js
+++
@@ -1,5 +0,0 @@
-module.exports = function objectToKeyValueArray(obj) {
-  // Use Object.entries to get an array of [key, value] pairs from the object.
-  // Then map each pair to an object with "key" and "value" properties.
-  return Object.entries(obj).map(([key, value]) => ({ key, value }));
-}
