c27300a640b1e1a0a593008d23e9418f68ef74c7
Paul Smith <mail@paulsmith.site>
2025-04-15 21:33:37 +0100

    component docs update

---
+++ app/lib/filters/highlight.js
@@ -0,0 +1,6 @@
+hljs = require('highlight.js');
+
+module.exports = function(code, lang='xml') {
+  return hljs.highlight(code, {language: lang}).value
+
+}
--- app/lib/filters/quoteIt.js
+++
@@ -1,8 +0,0 @@
-module.exports = function quoteIt(input) {
-	// Check if input is a string (this condition also covers string primitives)
-	if (typeof input === 'string' || input instanceof String) {
-		return `'${input}'`; // Return the string wrapped in single quotes
-	}
-	// Return the argument as is if it's not a string
-	return input;
-};
