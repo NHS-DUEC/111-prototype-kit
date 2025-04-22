09d29f26a5eaafb322584d1397181341a594f589
Paul Smith <mail@paulsmith.site>
2025-04-22 18:01:09 +0100

    workaround for embed with blocks

--- app/lib/filters/callMacroByName.js
+++ app/lib/filters/callMacroByName.js
@@ -11,35 +11,23 @@ function callByName(input, ...args) {
   let macroObj;
   let macroName;
   let macroArgs;
-
   // Case 1: input is macros object
   if (input && typeof input === 'object') {
     macroObj = input;
     macroName = args.shift();
     macroArgs = args;
-
   // Case 2: input is macro name string
   } else if (typeof input === 'string') {
     macroName = input;
-
-    // If first arg is a context var name mapping to an object, use it
-    if (args.length > 0 && typeof args[0] === 'string' && this.ctx && typeof this.ctx[args[0]] === 'object') {
-      macroObj = this.ctx[args.shift()];
-    } else {
-      // Default to `macros` in context
-      macroObj = (this.ctx && this.ctx.macros) || {};
-    }
+    macroObj = this.ctx;
     macroArgs = args;
-
   } else {
     throw new Error('callByName filter requires a macro object or macro name string');
   }
-
   const fn = macroObj[macroName];
   if (typeof fn !== 'function') {
     throw new Error(`callByName: Macro '${macroName}' is not defined on the provided object`);
   }
-
   const result = fn(...macroArgs);
   return new nunjucks.runtime.SafeString(result.toString());
 }
