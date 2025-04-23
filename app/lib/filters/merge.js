e5be4fd8622fff14cb051df1110e4a7f6bdbc365
Paul Smith <mail@paulsmith.site>
2025-04-23 17:41:27 +0100

    tidy variable names

--- app/lib/filters/merge.js
+++ app/lib/filters/merge.js
@@ -1,7 +1,7 @@
 // https://jhildenbiddle.github.io/mergician/
 const { mergician } = require('mergician');
 
-module.exports = function objectMerge(...objects) {
+module.exports = function merge(...objects) {
 	return mergician({
 		appendArrays: false,
 	})(...objects);
