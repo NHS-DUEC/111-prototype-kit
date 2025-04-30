fd8709079ea71ba04e3771f18617d474bff86dd8
Paul Smith <mail@paulsmith.site>
2025-04-30 13:47:18 +0100

    adds layouts for mod-zero and start

--- app/lib/filters/objectMerge.js
+++
@@ -1,8 +0,0 @@
-// https://jhildenbiddle.github.io/mergician/
-const { mergician } = require('mergician');
-
-module.exports = function objectMerge(...objects) {
-	return mergician({
-		appendArrays: false,
-	})(...objects);
-};
