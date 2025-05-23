60c56eaf6dd1701c4b43da8c0a17642b85b912f7
Paul Smith <mail@paulsmith.site>
2025-05-23 14:31:24 +0100

    documents merge filter

--- app/lib/filters/merge.js
+++ app/lib/filters/merge.js
@@ -1,3 +1,12 @@
+/**
+ * Merges multiple objects into a single object using the `mergician` library.
+ * Arrays are not appended; later values overwrite earlier ones.
+ *
+ * @function
+ * @param {...Object} objects - The objects to merge.
+ * @returns {Object} The merged object.
+ */
+
 // https://jhildenbiddle.github.io/mergician/
 const { mergician } = require('mergician');
 
