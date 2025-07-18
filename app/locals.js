44b87d30318a173e9e45816fb1e93418705fc70b
Paul Smith <mail@paulsmith.site>
2025-07-18 20:05:48 +0100

    Updated variable name

--- app/locals.js
+++ app/locals.js
@@ -1,7 +1,7 @@
 module.exports = (config) => (req, res, next) => {
 	res.locals.serviceName = config.serviceName;
 	res.locals.debug = config.debug;
-	res.locals.componentDoc = config.displayComponent;
-  res.locals.codeBlock = config.codeBlock;
+	res.locals.componentDoc = config.displayComponentPath;
+  res.locals.codeBlock = config.codeBlockPath;
 	next();
 };
