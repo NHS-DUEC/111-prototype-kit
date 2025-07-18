9536f8eaed5705c6e4a89bc6bf8d4d6a1dd7c029
Paul Smith <mail@paulsmith.site>
2025-07-18 19:36:03 +0100

    remove example routes

--- app/routes/version-1.js
+++ app/routes/version-1.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const utils = require('../lib/utils');
 
+// example of a redirect based on an answer to a question
 router.post('/age', function(req, res, next){
   var age = req.session.data['age'];
   if(age >= 5) {
@@ -13,24 +14,6 @@ router.post('/age', function(req, res, next){
   } else {
     console.log('Under 5')
     return res.redirect('check-symptoms-under-5');
-  }
-});
-
-router.post('/where-are-you-now', function(req, res){
-  var currentLocationPostcode = req.session.data.location.current;
-  if (utils.containsSubstring(currentLocationPostcode, 'LS14')) {
-    return res.redirect('age')
-  } else {
-    return res.redirect('age')
-  }
-});
-
-router.post('/age-pilot', function(req, res){
-  var age = req.session.data.age;
-  if (age <= 1) {
-    return res.redirect('under-1-notice')
-  } else {
-    return res.redirect('sex')
   }
 });
 
