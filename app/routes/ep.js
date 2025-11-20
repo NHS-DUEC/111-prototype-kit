d5d235c7692ad4754574f7fcd70525966a5b089f
Paul Smith <mail@paulsmith.site>
2025-11-20 21:39:28 +0000

    adds version 1

--- app/routes/ep.js
+++ app/routes/ep.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const utils = require('../lib/utils');
 
-// example of a redirect based on an answer to a question
+// ##################################################################
+// original flow (compressed version)
+// ##################################################################
+
 router.post('/questions-flow/original/was-a-repeat-prescription-requested', function(req, res){
   var answer = req.session.data['didYouSendARepeatPrescriptionRequestToYourGpSurgery'];
   if(answer == 'yes') {
@@ -18,5 +21,26 @@ router.post('/questions-flow/original/what-happened-to-your-repeat-rx-request', 
   var answer = req.session.data['whatHappenedToYourRepeatPrescriptionRequest'];
   next();
 });
+
+// ##################################################################
+// verison 1
+// ##################################################################
+
+router.post('/questions-flow/version-1/was-a-repeat-prescription-requested', function(req, res){
+  var answer = req.session.data['didYouSendARepeatPrescriptionRequestToYourGpSurgery'];
+  if(answer == 'yes') {
+    console.log('Redirecting to what-happened-to-your-repeat-rx-request');
+    return res.redirect('what-happened-to-your-repeat-rx-request');
+  } else {
+    console.log('Redirecting to why-no-repeat-prescription-request');
+    return res.redirect('why-no-repeat-prescription-request');
+  }
+});
+
+router.post('/questions-flow/version-1/what-happened-to-your-repeat-rx-request', function(req, res, next){
+  var answer = req.session.data['whatHappenedToYourRepeatPrescriptionRequest'];
+  next();
+});
+
 
 module.exports = router;
