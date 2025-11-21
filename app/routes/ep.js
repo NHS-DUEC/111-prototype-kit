4bc083fec7668cd085797c0968cdd5373bbff15d
Paul Smith <mail@paulsmith.site>
2025-11-21 14:43:36 +0000

    lots of content changes in new flow

--- app/routes/ep.js
+++ app/routes/ep.js
@@ -27,7 +27,9 @@ router.post('/questions-flow/original/what-happened-to-your-repeat-rx-request', 
 // ##################################################################
 
 router.post('/questions-flow/version-1/was-a-repeat-prescription-requested', function(req, res){
+  req.session.data.answers = req.session.data.answers || {};
   var answer = req.session.data['didYouSendARepeatPrescriptionRequestToYourGpSurgery'];
+  req.session.data.answers.wasARepeatPrescriptionRequested = answer;
   if(answer == 'yes') {
     console.log('Redirecting to what-happened-to-your-repeat-rx-request');
     return res.redirect('what-happened-to-your-repeat-rx-request');
@@ -38,7 +40,14 @@ router.post('/questions-flow/version-1/was-a-repeat-prescription-requested', fun
 });
 
 router.post('/questions-flow/version-1/what-happened-to-your-repeat-rx-request', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
   var answer = req.session.data['whatHappenedToYourRepeatPrescriptionRequest'];
+  req.session.data.answers.whatHappenedToYourRepeatPrescriptionRequest = answer;
+
+  if(answer == 'not-available-at-pharmacy') {
+    return res.redirect('pharmacy-out-of-stock-question');
+  }
+
   next();
 });
 
