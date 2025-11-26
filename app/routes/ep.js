c62dc1774e2c83fc54131dd1d10729a84db53c48
Paul Smith <mail@paulsmith.site>
2025-11-26 13:56:54 +0000

    adds do-dont list style to dynamic content

--- app/routes/ep.js
+++ app/routes/ep.js
@@ -63,5 +63,41 @@ router.post('/questions-flow/version-1/pharmacy-out-of-stock-question', function
   next();
 });
 
+// ##################################################################
+// verison 2
+// ##################################################################
+
+router.post('/questions-flow/version-2/was-a-repeat-prescription-requested', function(req, res){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['didYouSendARepeatPrescriptionRequestToYourGpSurgery'];
+  req.session.data.answers.wasARepeatPrescriptionRequested = answer;
+  if(answer == 'yes') {
+    console.log('Redirecting to what-happened-to-your-repeat-rx-request');
+    return res.redirect('what-happened-to-your-repeat-rx-request');
+  } else {
+    console.log('Redirecting to why-no-repeat-prescription-request');
+    return res.redirect('why-no-repeat-prescription-request');
+  }
+});
+
+router.post('/questions-flow/version-2/what-happened-to-your-repeat-rx-request', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['whatHappenedToYourRepeatPrescriptionRequest'];
+  req.session.data.answers.whatHappenedToYourRepeatPrescriptionRequest = answer;
+  next();
+});
+
+router.post('/questions-flow/version-2/pharmacy-out-of-stock-question', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['didYourPharmacyTryToFindYourMedicineFromElsewhere'];
+  req.session.data.answers.didYourPharmacyTryToFindYourMedicineFromElsewhere = answer;
+
+  if(answer == 'no') {
+    return res.redirect('pharmacy-out-of-stock-info');
+  } else {
+    return res.redirect('when-meds-due');
+  }
+  next();
+});
 
 module.exports = router;
