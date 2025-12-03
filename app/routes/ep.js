91aa320aaaae7c2329dc4e824e1aecc3463b641a
Paul Smith <mail@paulsmith.site>
2025-12-03 10:54:09 +0000

    updates not yet approved

--- app/routes/ep.js
+++ app/routes/ep.js
@@ -100,4 +100,41 @@ router.post('/questions-flow/version-2/why-no-repeat-prescription-request', func
   next();
 });
 
+// ##################################################################
+// verison 3
+// ##################################################################
+
+router.post('/questions-flow/version-3/was-a-repeat-prescription-requested', function(req, res){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['didYouSendARepeatPrescriptionRequestToYourGpSurgery'];
+  req.session.data.answers.wasARepeatPrescriptionRequested = answer;
+  if(answer == 'yes') {
+    console.log('Redirecting to what-happened-to-your-repeat-rx-request');
+    return res.redirect('what-happened-to-your-repeat-rx-request');
+  } else {
+    console.log('Redirecting to why-no-repeat-prescription-request');
+    return res.redirect('why-no-repeat-prescription-request');
+  }
+});
+
+router.post('/questions-flow/version-3/what-happened-to-your-repeat-rx-request', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['whatHappenedToYourRepeatPrescriptionRequest'];
+  req.session.data.answers.whatHappenedToYourRepeatPrescriptionRequest = answer;
+  if(answer == 'not-sure') {
+    return res.redirect('when-meds-due');
+  }
+  next();
+});
+
+router.post('/questions-flow/version-3/why-no-repeat-prescription-request', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['whyDidYouNotSendARepeatPrescriptionRequest'];
+  req.session.data.answers.whyDidYouNotSendARepeatPrescriptionRequest = answer;
+  if(answer == 'other') {
+    return res.redirect('when-meds-due');
+  }
+  next();
+});
+
 module.exports = router;
