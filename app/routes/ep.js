c1e5e92da9d1f02114ac30881a0ef6b1fdb9c9c1
Paul Smith <mail@paulsmith.site>
2025-12-19 14:59:04 +0000

    adds third person flow and iterated content

--- app/routes/ep.js
+++ app/routes/ep.js
@@ -178,6 +178,22 @@ router.post('/questions-flow/version-4/why-no-repeat-prescription-request', func
 // verison 5
 // ##################################################################
 
+router.post('/questions-flow/version-5/who-needs-help', function(req, res){
+  req.session.data.answers = {};
+  var answer = req.session.data['whoNeedsHelp'];
+  req.session.data.answers.whoNeedsHelp = answer;
+  if(answer == 'me') {
+    console.log('1st person');
+    return res.redirect('was-a-repeat-prescription-requested');
+  } else {
+    console.log('3rd person');
+    return res.redirect('third-person/was-a-repeat-prescription-requested');
+  }
+});
+
+// Version 5 - 1st person flow
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 router.post('/questions-flow/version-5/was-a-repeat-prescription-requested', function(req, res){
   req.session.data.answers = {};
   var answer = req.session.data['didYouSendARepeatPrescriptionRequestToYourGpSurgery'];
@@ -205,6 +221,42 @@ router.post('/questions-flow/version-5/why-no-repeat-prescription-request', func
   req.session.data.answers = req.session.data.answers || {};
   var answer = req.session.data['whyDidYouNotSendARepeatPrescriptionRequest'];
   req.session.data.answers.whyDidYouNotSendARepeatPrescriptionRequest = answer;
+  if(answer == 'other' || answer == 'not-registered-with-a-gp') {
+    return res.redirect('when-meds-due');
+  }
+  next();
+});
+
+// Version 5 - third person flow
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+router.post('/questions-flow/version-5/third-person/was-a-repeat-prescription-requested', function(req, res){
+  req.session.data.answers = {};
+  var answer = req.session.data['didTheySendARepeatPrescriptionRequestToYourGpSurgery'];
+  req.session.data.answers.wasARepeatPrescriptionRequested = answer;
+  if(answer == 'yes') {
+    console.log('Redirecting to what-happened-to-repeat-rx-request');
+    return res.redirect('what-happened-to-repeat-rx-request');
+  } else {
+    console.log('Redirecting to why-no-repeat-prescription-request');
+    return res.redirect('why-no-repeat-prescription-request');
+  }
+});
+
+router.post('/questions-flow/version-5/third-person/what-happened-to-repeat-rx-request', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['whatHappenedToTheirRepeatPrescriptionRequest'];
+  req.session.data.answers.whatHappenedToTheirRepeatPrescriptionRequest = answer;
+  if(answer == 'not-sure') {
+    return res.redirect('when-meds-due');
+  }
+  next();
+});
+
+router.post('/questions-flow/version-5/third-person/why-no-repeat-prescription-request', function(req, res, next){
+  req.session.data.answers = req.session.data.answers || {};
+  var answer = req.session.data['whyDidTheyNotSendARepeatPrescriptionRequest'];
+  req.session.data.answers.whyDidTheyNotSendARepeatPrescriptionRequest = answer;
   if(answer == 'other' || answer == 'not-registered-with-a-gp') {
     return res.redirect('when-meds-due');
   }
