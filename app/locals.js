8d4618bad66fd7c364d9f0b7b3d890aa72eb7637
Paul Smith <mail@paulsmith.site>
2026-02-20 12:41:07 +0000

    updates the example in locals.js

--- app/locals.js
+++ app/locals.js
@@ -1,10 +1,21 @@
 const config = require("./config");
+const applyLocals = require("./lib/locals");
 
 module.exports = function (req, res, next) {
-  res.locals.isFirstPerson = req.session?.data?.pov === "first-person";
-  res.locals.layouts = config.layouts;
-  res.locals.debug = config.debug;
-  res.locals.componentDoc = config.displayComponentPath;
-  res.locals.codeBlock = config.codeBlockPath;
+  // these are default 111 prototype kit locals, which are applied to every request
+  applyLocals(req, res, config);
+
+  // ###############################################################
+  // add your own custom locals here, for example:
+  // res.locals.myValue = "some value";
+  // which can be used in your views/templates as {{ myValue }}
+  // ###############################################################
+
+  res.locals.myValue = "some value";
+
+  // ###############################################################
+  // keep this at the end of the function to ensure all locals are
+  // applied before rendering views
+  // ###############################################################
   next();
 };
